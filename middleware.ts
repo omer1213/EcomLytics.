a30d70dfcc80b7b94fc369685eb8b26b797b2045
omer1213 <132270167+omer1213@users.noreply.github.com>
2025-06-24 12:22:11 +0500

    first project comita

---
+++ middleware.ts
@@ -0,0 +1,35 @@
+
+
+// import { clerkMiddleware } from '@clerk/nextjs/server';
+
+// export default clerkMiddleware();
+
+// export const config = {
+//   matcher: [
+//     // Protect everything except Clerk and static files
+//     '/((?!_next|.*\\..*|sign-in|sign-up|api/webhooks|favicon.ico).*)',
+//   ],
+// };
+
+
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+// Add your specific routes to be protected
+const isProtectedRoute = createRouteMatcher([
+    // '/',
+    '/aiwriter',
+    '/chat/[id]',
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+    if (isProtectedRoute(req)) {
+        await auth.protect(); // ✅ No parentheses!
+    }
+});
+
+export const config = {
+    matcher: [
+        '/((?!_next|.*\\..*|sign-in|sign-up|api/webhooks|favicon.ico).*)',
+    ],
+};
